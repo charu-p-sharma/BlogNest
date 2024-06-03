a9f376d4478032bcb8a5badac44e87db67c50bad
charu <charupsharma5@gmail.com>
2024-06-03 14:20:32 +0530

    auth routes and controller added

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import router from "./routes/user.route.js"
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 dotenv.config();
 
 mongoose
@@ -15,7 +16,8 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
 
-app.use("/api/user", router)
+app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
 
 // app.get("/test", (req, res) => {
 //     res.json({ message: "Api is working" });
