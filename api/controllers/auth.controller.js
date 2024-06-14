a6b828f95f7e2ef619d81eac100276e8776a4eb2
charu <charupsharma5@gmail.com>
2024-06-14 10:55:36 +0530

    middleware for signup added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcrypt";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     if (!username || !email || !password || username === '' || email === '' || password === '') {
         return res.status(400).json({ message: "All fields are required" });
@@ -21,6 +21,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json({ message: "Signup successful" })
     } catch (error) {
-        res.status(500).json({ messaage: error.message });
+        next(error);
     }
 }
--- api/index.js
+++ api/index.js
@@ -24,3 +24,14 @@ app.use("/api/auth", authRouter)
 app.get("/test", (req, res) => {
     res.json({ message: "Api is working" });
 });
+
+//middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
